eefdd7796230edee22ecf8262905aadc5c8a2614
Ondrej Bilka <neleai@seznam.cz>
2011-01-16 09:55:18 +0100

    working on c runtime

--- ctest.rb
+++ ctest.rb
@@ -25,6 +25,7 @@ o.close
 require 'ctranslator.rb'
 #t = And[And[Act["aaa"],Act["bbb"],And[Act["ccc"],Or[Or[And[And[[Act['a']]]]]]]]]
 t= [Grammar[{:rules=>Rule[{:name=>"ada",:body=>And[Act["aaa"],Act[{:ary=>["bbb"],:pred=>true}],Or[Act["a"],Act[{:ary=>["bbb"],:pred=>true}]],Many[Act[{:ary=>["bbb"],:pred=>true}]] ]  }]}]]
+t=[Grammar[{:name=>"Test", :rules=>[Rule[{:name=>"ada",:body=>And[Apply['ee'],Char["a"],Or[And[Char["b"],Act["puts 42"]],And[Char["c"],Act["puts 43"]]]]  }],Rule[{:name=>"ee",:body=>And[Char["f"],Act["puts 23"]]}]]}]]
 t= AmethystRBTranslator.new.parse(:root,t)
 puts t.rbcode
 res= AmethystCTranslator.new.parse(:trans,[t])
