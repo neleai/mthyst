94bb93fcd578e450d421ec41e51e59c0296eddf7
Ondrej Bilka <neleai@seznam.cz>
2011-07-03 02:19:41 +0200

    setting variable directly

--- ctest.rb
+++ ctest.rb
@@ -38,9 +38,12 @@ par= AmethystParser.new.parse(:igrammar, File.new("amethyst/amethyst.ame").read)
 	t=AmethystTranslator.new
 	a=Analyze_Variables2.new
 	opt=o.parse(:itrans,par)
-	opt[0].rules=opt[0].rules[5,1]
+	opt[0].rules=opt[0].rules[18,1]
+#	opt[0].rules[0].body=opt[0].rules[0].body[0]
+pp opt
 	opt[0].rules=opt[0].rules.map{|r| a.parse(:root,r)}
-#	opt=a.parse(:root,opt)
+
+#	opt[0].rules[0].body=opt[0].rules[0].body[4][0].expr
 t=opt
 pp t
 t= AmethystRBTranslator.new.parse(:root,t)
