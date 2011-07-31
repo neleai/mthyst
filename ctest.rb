b54282503903a77e093aa0e004cc1d0901ffec5c
Ondrej Bilka <neleai@seznam.cz>
2011-07-31 20:33:50 +0200

    processing ruby arguments

--- ctest.rb
+++ ctest.rb
@@ -12,15 +12,13 @@ def a2ruby(s)
 	ruby=t.parse(:itrans,opt)
 end
 
-
+if true
 o=File.open("ctranslator2.rb","w")
-
 o.puts a2ruby(File.new("amethyst/ctranslator2.ame").read)
-
 o.close
-#puts $opt.inspect
+end
 require 'ctranslator2.rb'
-g=[Grammar[{:name=>"Foo",:rules=>[Rule[{:name=>"foo",:args=>[],:body=>Seq[Apply["bar"],Apply["x"],Set[{:name=>"result",:expr=>Act["42"]}]]}]]}]]
+g=[Grammar[{:name=>"Foo",:rules=>[Rule[{:name=>"foo",:args=>[],:body=>Seq[Apply["bar"],Apply["x"],Set[{:name=>"result",:expr=>Act["42"]}]]}],Rule[{:name=>"a",:args=>[],:body=>Seq[Act[Args["u=",Local["a"]]]]}]]}]]
 s=AmethystCTranslator.new.parse(:trans,g)
 puts s
 File.open("c/foo.c","w"){|f| f.puts s}
