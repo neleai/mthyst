36f5fa14633c82569155ba60b479c045c5307f4e
Ondrej Bilka <neleai@seznam.cz>
2011-08-01 20:08:49 +0200

    trying to translate amethyst.ame to c

--- ctest.rb
+++ ctest.rb
@@ -9,6 +9,7 @@ def a2ruby(s)
 	[AmethystOptimizer2,Analyze_Variables2,Move_Assignments,AmethystOptimizer2].each{|p|
 		opt=p.new.parse(:itrans,opt)
 	}
+	$opt=opt
 	ruby=t.parse(:itrans,opt)
 end
 
@@ -18,7 +19,10 @@ o.puts a2ruby(File.new("amethyst/ctranslator2.ame").read)
 o.close
 end
 require 'ctranslator2.rb'
-g=[Grammar[{:name=>"Foo",:parent=>"Amethyst",:rules=>[Rule[{:name=>"foo",:args=>[],:body=>Seq[Lookahead[Apply["bar"]],Apply["x"],Set[{:name=>Local["result"],:expr=>Act["42"]}]]}],Rule[{:name=>"a",:args=>[],:body=>Seq[Act[Args["u=",Local["a"]]]]}], Rule[{:name=>"t",:args=>[],:body=>Seq[Or[Apply["spaces"],Apply["seq",Args["'x'"]]]]}] ]}]]
-s=AmethystCTranslator.new.parse(:trans,g)
-puts s
+#g=[Grammar[{:name=>"Foo",:parent=>"Amethyst",:rules=>[Rule[{:name=>"foo",:args=>[],:body=>Seq[Lookahead[Apply["bar"]],Apply["x"],Set[{:name=>Local["result"],:expr=>Act["42"]}]]}],Rule[{:name=>"a",:args=>[],:body=>Seq[Act[Args["u=",Local["a"]]]]}], Rule[{:name=>"t",:args=>[],:body=>Seq[Or[Apply["spaces"],Apply["seq",Args["'x'"]]]]}] ]}]]
+a2ruby(File.new("amethyst/amethyst.ame").read)
+
+	puts $opt.inspect
+	s=AmethystCTranslator.new.parse(:trans,$opt)
+	puts s
 File.open("c/foo.c","w"){|f| f.puts s}
