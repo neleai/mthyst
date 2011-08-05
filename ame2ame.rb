9a11bfcfba4afad7e3db97851f91a6d439ef95f1
Ondrej Bilka <neleai@seznam.cz>
2011-08-05 16:14:38 +0200

    and numbering of locals in c

--- ame2ame.rb
+++ ame2ame.rb
@@ -6,10 +6,10 @@ require 'amethyst'
 #require 'compiled/parser'
 #require 'compiled/optimizer_null'
 #require 'compiled/optimizer_and_or'
-#require 'c/Amethyst'
+require 'c/Amethyst'
 #require 'c/AmethystOptimizer'
 #require 'c/AmethystOptimizer2'
-#require 'c/Traverser'
+require 'c/Traverser'
 
 def a2ruby(s)
 	par=AmethystParser.new.parse(:igrammar,s)
