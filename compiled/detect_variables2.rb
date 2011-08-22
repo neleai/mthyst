1df08b614df56f7bf82f9699393f453d76122600
Ondrej Bilka <neleai@seznam.cz>
2011-08-22 19:56:20 +0200

    next modification

--- compiled/detect_variables2.rb
+++ compiled/detect_variables2.rb
@@ -203,7 +203,7 @@ def visit_Remap_Actscb_2(bind)
 (bind[:i_1].is_a?(bind[:cls_1])) || FAIL
 end
 def visit_Remap_Actscb_3(bind)
-(@src.pure ? PureAct : Act)[@src.ary,@src.pred]
+(@src.pure ? PureAct[@src.ary] : Act[@src.ary,@src.pred])
 end
 
 end
