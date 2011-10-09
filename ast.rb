7e55c86d9066e594dd6a647541ab7e62232218d4
Ondrej Bilka <neleai@seznam.cz>
2011-10-09 16:20:27 +0200

    refactored ccode

--- ast.rb
+++ ast.rb
@@ -144,18 +144,15 @@ end
 $constno=0
 class CAct
 	def pure;	true;	end
-	def ccode2	#rewrite in amethyst
-		return "rb_ary_new3(0)" if ary[0].is_a?(Array)
-		return "Q#{ary[0].inspect}" if [true,false,nil].include?(ary[0])
-		ary[0]
-	end
-	def ccode
+	def ccode #rewrite in amethyst
 		$constno+=1
+		return [nil,nil,"rb_ary_new3(0)"] if ary[0].is_a?(Array)
+    return [nil,nil,"Q#{ary[0].inspect}"] if [true,false,nil].include?(ary[0])
 		return ["VALUE c_#{$constno};","c_#{$constno}=rb_const_get(rb_cObject, rb_intern(\"#{ary[0].inspect}\"));","c_#{$constno}"] if ary[0].is_a?(Class)
 		#ugly but needed for arbitrary precision(alternatively emit int2fix when fits fixnum range)
 		return ["VALUE c_#{$constno};","c_#{$constno}=rb_funcall(rb_str_new2(\"#{ary[0]}\"),rb_intern(\"to_i\"),0);#{gc_mark_var("c_#{$constno}")}","c_#{$constno}"] if ary[0].is_a? Integer
 		return ["VALUE c_#{$constno};","c_#{$constno}=rb_str_new2(#{ary[0].inspect});#{gc_mark_var("c_#{$constno}")}","rb_obj_clone(c_#{$constno})"] if ary[0].is_a?(String)
-		[nil,nil,ccode2]
+		[nil,nil,ary[0]]
 	end
 end
 class Act
