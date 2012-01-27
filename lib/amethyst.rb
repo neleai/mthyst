70db083d44067d353517da7dd4d0f587d20b89e3
Ondrej Bilka <neleai@seznam.cz>
2012-01-27 21:59:31 +0100

    moved lib/runtime to lib/amethyst

--- lib/amethyst.rb
+++ lib/amethyst.rb
@@ -1,45 +1,47 @@
 Amethyst_path=File.expand_path(File.dirname(__FILE__)+"/..")
 $: << Amethyst_path
-['graph','utils','ast','runtime'].each{|r|
+['graph','utils','ast'].each{|r|
 	require Amethyst_path+"/lib/#{r}"
 }
+FAIL=Object.new
+class << FAIL
+	def inspect;		"FAIL";	end
+	alias_method :to_s,:inspect
+end
+
+class AmethystLambda
+	def initialize(call,parser,bind)
+		@call,@parser,@bind=call,parser,bind
+	end
+	def call
+		@parser.send(@call,@bind)
+	end
+end
+
+class AmethystCore
+	def apply(rule)
+		if rule.is_a?(String) || rule.is_a?(Symbol)
+			send(rule)
+		else
+			rule.call
+		end
+	end
+end
+class <<AmethystCore
+	def parse(rule,input)
+		self.class.new.parse(rule,input)
+	end
+end
+require Amethyst_path+"/compiled/#{RUBY_VERSION}/Ame.so"
+
 GC_mark=[]#mark for gc
 def gc_mark_var(var)
 	"rb_ary_push(rb_const_get(rb_cObject,rb_intern(\"GC_mark\")),#{var});"
 end
 class Amethyst < AmethystCore	
-	def self::load(g)
-    evalgrammar(grammar(filename))	
-	end
-	def self::grammar(g)
-	end
-	def self::evalgrammar(g)
-	end
-	class Settings
-		def self.profiling=(a)
-			@profiling=a
-		end
-		def self.profiling
-			@profiling
-		end
-		def self.compile_for=(a)
-			@compile_for=a
-		end
-		def self.compile_for
-			@compile_for
-		end
-		def self.cflags=(a)
-			@cflags=a
-		end
-		def self.cflags
-			@cflags
-		end
-		def self.debug=(a)
-			@debug=a
-		end
-		def self.debug
-			@debug
-		end
+	class Settings;end
+	class <<Settings
+		attr_accessor :profiling,:compile_for,:cflags,:debug
 	end
 end
 require Amethyst_path+"/settings.rb"
