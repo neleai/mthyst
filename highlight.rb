cd0559f1b51673bd156ab8213992edaf2c601201
Ondrej Bilka <neleai@seznam.cz>
2012-03-05 11:32:27 +0100

    highligth by annotating parser

--- highlight.rb
+++ highlight.rb
@@ -1,6 +1,6 @@
 require './lib/amethyst'
-def ast2ame_compiled_by; "42";end
-["ast2ame"].each{|f|
+def parser_highlight_compiled_by; "42";end
+["amethyst","parser_highlight"].each{|f|
 Compiler::compile("amethyst/#{f}.ame","compiled/#{f}.rb",f)
 
 require "./compiled/#{f}"
@@ -11,14 +11,25 @@ wrap="amethyst Foo{
   bar= foo:bar baz[ aa ] {2+2}=>foo
 }"
 wrap=File.new("amethyst/amethyst.ame").read
-s=AmethystParser.new.parse(:file,wrap)
-puts s.inspect
-
-r= Syntax_Highligth.new.parse(:root,s)
-f=File.new("test.html","w")
-def synchronize_whitespace(s,trans)
-end
+$colors=[]
+AmethystParser_Highligth.new.parse(:file,wrap)
+r=""
+wrap.size.times{|i|
+  c=wrap[i]
+  case c
+  when "<"
+    c="&lt;"
+  when ">"
+    c="&gt;"
+  when "&"
+    c="&amp;"
+  end
+  c="<span style=\"color:#{$colors[i]}\">#{c}</span>" if $colors[i]
+  
+  r<<c
+}
 puts r.inspect
+f=File.new("test.html","w")
 f.puts "<body bgcolor=black> <pre><div style=\"color:white\">"
 f.puts(r)
 f.puts "</pre></div></body>"
