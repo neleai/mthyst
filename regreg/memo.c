4df54b8628295873ed8f45969138ef5071328fe9
Ondrej Bilka <neleai@seznam.cz>
2013-03-02 09:09:07 +0100

    formating

--- regreg/memo.c
+++ regreg/memo.c
@@ -6,7 +6,7 @@ typedef struct {
 static memo_entry *memos;static long memo_no;
 static void memo_set(Global *g,char *str,exp *e ,exp *memo, memo_s set){int i;
   for(i=0;i<memo_no;i++){
-    if (memos[i].str==str&& memos[i].e==e&& memos[i].memo==memo)
+    if (memos[i].str==str && memos[i].e==e && memos[i].memo==memo)
       return ;
   }
   if (!((memo_no&(memo_no-1)))) memos=realloc(memos,sizeof(memo_entry)*2*(memo_no+1));
@@ -15,7 +15,7 @@ static void memo_set(Global *g,char *str,exp *e ,exp *memo, memo_s set){int i;
 }
 static memo_s memo_get(Global *g,char *str,exp *e, exp *memo){int i;
   for(i=0;i<memo_no;i++){
-    if (memos[i].str==str&& memos[i].e==e&& memos[i].memo==memo)
+    if (memos[i].str==str && memos[i].e==e && memos[i].memo==memo)
       return memos[i].set;
   }
   memo_s not_present;
